685891c320806b023747b630907bdc74d029e6bf
zAHEEr <ujoshua976@gmail.com>
2024-07-09 23:33:24 +0100

    added files

---
+++ src/product.js
@@ -0,0 +1,37 @@
+import prod1 from './Components/assets/headset.png'
+import prod2 from './Components/assets/headset2.png'
+import prod3 from './Components/assets/headset3.png'
+import prod4 from './Components/assets/headset4.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Industrial Bed',
+        price: 300.0,
+        productImage: prod1,
+        description: 'Organic Cotton, fairtrades certified'
+    },
+    {
+        id : 2,
+        productName: 'Industrial Bed',
+        price: 300.0,
+        productImage: prod2,
+        description: 'Organic Cotton, fairtrades certified'
+    },
+    {
+        id : 3,
+        productName: 'Industrial Bed',
+        price: 300.0,
+        productImage: prod3,
+        description: 'Organic Cotton, fairtrades certified'
+    },
+    {
+        id : 4,
+        productName: 'Industrial Bed',
+        price: 300.0,
+        productImage: prod4,
+        description: 'Organic Cotton, fairtrades certified'
+    },
+
+  
+]
